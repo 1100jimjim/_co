6909bb2bce583df5e3389e9bb6e4d46614876c49
1100jimjim <161785835+1100jimjim@users.noreply.github.com>
2024-12-17 22:06:33 +0800

    Update Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,30 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@n
+M=0
+@R0
+D=M
+@1
+M=D
+(MainLoop)
+@1
+D=M
+@Exit
+D;JEQ
+@R1
+D=M
+@n
+M=M+D
+@1
+M=M-1
+@MainLoop
+0;JMP
+(Exit)
+@n
+D=M
+@R2
+M=D
+(End)
+0;JMP
